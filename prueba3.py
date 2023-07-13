f09f12a3342dab44021df777d6b9757f8e82dbf6
AlexCapis <alexmarza17@gmail.com>
2023-07-13 17:22:51 +0200

    actualizo

---
+++ prueba3.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, render_template
+from langchain.agents import load_tools, initialize_agent
+from langchain.llms import OpenAI
+import os
+
+os.environ["OPENAI_API_KEY"] = "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"
+os.environ["SERPAPI_API_KEY"] = "6cdddecde869790ad6c4ed5c449addd08f39948071e3802d9bbe07e86bbcf276"
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+@app.route("/")
+def hello():
+    return render_template('preguntas.html')
+
+
+@app.route('/api/generar-respuesta', methods=['POST'])
+def generar_respuesta():
+    pregunta = input("Introduce tu consulta")
+    print(pregunta)
+    try:
+        # Load the model
+        llm = OpenAI()
+
+        # Load in some tools to use
+        tools = load_tools(["serpapi", "llm-math"], llm=llm)
+
+        # Initialize the agent with the tools, language model, and agent type
+        agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=False)
+
+        # Use the agent to generate a response
+        respuesta = agent.run(pregunta)
+
+
+        # Acceso a la base de datos 
+        cursor = db.cursor()
+        cursor.connection.commit()
+        db = ''' '''
+        cursor.execute()
+
+
+        # Insertamos en la tabla elegida los datos obtenidos
+        insert_data = '''INSERT INTO GPT (FECHA, PREGUNTAS, RESPUESTAS) VALUES ('%s', '%s', '%s')''' % pregunta_respuesta
+        crsr.execute(insert_data)
+
+        # Devuelve la respuesta generada en formato JSON
+        db.commit()
+        db.close()
+        return jsonify({'respuesta': respuesta})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 50
+
+
+app.run()
