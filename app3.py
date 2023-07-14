2a2d9dff9b678569da4070cce984f0c44123075d
erika guzman <de969500@gmail.com>
2023-07-14 09:09:34 +0200

    cambios

---
+++ app3.py
@@ -0,0 +1,65 @@
+from flask import Flask, jsonify, request
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.llms import OpenAI
+import pymysql
+import os
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# Configura las claves de API
+os.environ["OPENAI_API_KEY"] = "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"
+os.environ["SERPAPI_API_KEY"] = "6cdddecde869790ad6c4ed5c449addd08f39948071e3802d9bbe07e86bbcf276"
+
+# Crear una conexión a la base de datos
+def create_db_connection():
+    return pymysql.connect(
+        host='http://127.0.0.1:5000/',
+        user='your-username',
+        password='your-password',
+        database='your-database'
+    )
+
+# Cerrar la conexión a la base de datos
+def close_db_connection(connection):
+    if connection is not None:
+        connection.close()
+
+@app.route("/", methods=['GET'])
+def hello():
+    return "Bienvenido a la API del proyecto GPTGoogler"
+
+@app.route('/api/generar-respuesta', methods=['POST'])
+def generar_respuesta():
+    pregunta = request.json['pregunta']
+
+    try:
+        # Load the model
+        llm = OpenAI()
+
+        # Load in some tools to use
+        tools = load_tools(["serpapi", "llm-math"], llm=llm)
+
+        # Initialize the agent with the tools, language model, and agent type
+        agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=False)
+
+        # Use the agent to generate a response
+        respuesta = agent.run(pregunta)
+
+        # Guardar la pregunta y la respuesta en la base de datos
+        connection = create_db_connection()
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO preguntas_respuestas (pregunta, respuesta) VALUES (%s, %s)", (pregunta, respuesta))
+        connection.commit()
+
+        # Cerrar la conexión a la base de datos
+        close_db_connection(connection)
+
+        # Devuelve la respuesta generada en formato JSON
+        return jsonify({'respuesta': respuesta})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
