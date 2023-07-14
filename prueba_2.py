562c7075665ba374f3d4c1b87c60329f080f05cb
jaferdy <jaferdi5@gmail.com>
2023-07-14 10:17:12 +0200

    prueba_2.py con fechas

--- prueba_2.py
+++ prueba_2.py
@@ -1,53 +1,10 @@
-# from flask import Flask, jsonify
-# from langchain.agents import load_tools
-# from langchain.agents import initialize_agent
-# from langchain.llms import OpenAI
-# import os
-
-# os.environ["OPENAI_API_KEY"] = "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"
-# os.environ["SERPAPI_API_KEY"] = "6cdddecde869790ad6c4ed5c449addd08f39948071e3802d9bbe07e86bbcf276"
-
-# app = Flask(__name__)
-# app.config['DEBUG'] = True
-
-
-# @app.route("/", methods=['GET'])
-# def hello():
-#     return "Bienvenido a la API del proyecto GPTGoogler"
-
-
-# @app.route('/api/generar-respuesta', methods=['POST'])
-# def generar_respuesta():
-#     pregunta = input("Introduce tu consulta")
-
-#     try:
-#         # Load the model
-#         llm = OpenAI()
-
-#         # Load in some tools to use
-#         tools = load_tools(["serpapi", "llm-math"], llm=llm)
-
-#         # Initialize the agent with the tools, language model, and agent type
-#         agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=False)
-
-#         # Use the agent to generate a response
-#         respuesta = agent.run(pregunta)
-
-#         # Devuelve la respuesta generada en formato JSON
-#         return jsonify({'respuesta': respuesta})
-#     except Exception as e:
-#         return jsonify({'error': str(e)}), 50
-
-
-# app.run()
-
-
 from flask import Flask, jsonify
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
 from langchain.llms import OpenAI
 import pymysql
 import os
+import datetime
 
 os.environ["OPENAI_API_KEY"] = "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"
 os.environ["SERPAPI_API_KEY"] = "6cdddecde869790ad6c4ed5c449addd08f39948071e3802d9bbe07e86bbcf276"
@@ -76,7 +33,7 @@ def hello():
 
 @app.route('/api/generar-respuesta', methods=['POST'])
 def generar_respuesta():
-    pregunta = input("Introduce tu consulta")
+    pregunta = input("Introduce tu consulta: ")
 
     try:
         # Load the model
@@ -91,10 +48,12 @@ def generar_respuesta():
         # Use the agent to generate a response
         respuesta = agent.run(pregunta)
 
+        # Obtener la fecha actual
+        fecha_actual = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         # Guardar la pregunta y la respuesta en la base de datos
         connection = create_db_connection()
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO GPT (PREGUNTAS, RESPUESTAS) VALUES (%s, %s)", (pregunta, respuesta))
+        cursor.execute("INSERT INTO GPT (FECHA, PREGUNTAS, RESPUESTAS) VALUES (%s, %s, %s)", (fecha_actual, pregunta, respuesta))
         connection.commit()
 
         # Cerrar la conexión a la base de datos
@@ -107,7 +66,3 @@ def generar_respuesta():
 
 
 app.run()
-
-
-
-
